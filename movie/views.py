ae700ea61ab398a0dedf1cfd7c43c91a4159b674
Tomas Ospina <tospinai@eafit.edu.co>
2026-02-05 12:36:35 -0500

    Busqueda de peliculas.

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Tomas Ospina'})
+    SearchTerm = request.GET.get('SearchMovie')
+    if SearchTerm:
+        movies = Movie.objects.filter(title__icontains=SearchTerm)
+    else:
+        movies = Movie.objects.all
+    return render(request, 'home.html', {'SearchTerm':SearchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
